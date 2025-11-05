4442bf3835e63a0f4787081928b6fca2f9cab1db
DXC-0 <inscription-site-1@protonmail.com>
2025-11-05 00:55:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ PACKAGES=(
 
 dnf install --setopt=install_weak_deps=False -y "${PACKAGES[@]}"
 
-# Flatpak + bazaar install
+# Flatpak + Bazaar install
 
 dnf install -y flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -54,9 +54,18 @@ flatpak install -y flathub io.github.kolunmi.Bazaar
 
 dnf install -y https://nix-community.github.io/nix-installers/nix/x86_64/nix-multi-user-2.24.10.rpm
 
+# Non  root user
+
+NON_ROOT_USER=$(logname)
+USER_HOME=$(eval echo "~$NON_ROOT_USER")
+
 # Install dotfiles
 
-mv ./dotfiles/* $HOME/.config/
+mkdir -p $USER_HOME/.config/
+mv ./Dotfiles/* $USER_HOME/.config/
+git clone https://github.com/DXC-0/Flatery.git "$USER_HOME/Flatery"
+mkdir $USER_HOME/.icons/
+mv $USER_HOME/Flatery/* "$USER_HOME/.icons/"
 
 # Autologin
 
